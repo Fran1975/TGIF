40d267f64dbccd789a6ca2a0b651894ebebafb19
Francisco Jose Santiago Garrido <f.santiago.ubiqum@gmail.com>
2020-02-12 14:35:44 +0100

    TGIF 12022010

--- Exercises/ejemplos.js
+++
@@ -1,8 +0,0 @@
-let animals = ["dog","cat","pig","bird"]
-let developers = ["jack","alex","mary"]
-
-function showAnimals () {
-    for(var i=0; i < animals.length; i++) {
-        console.log(animals[i])
-    }
-}
